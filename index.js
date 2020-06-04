39c23770b9ad1c942668600aed18e3f6e9ab6ef8
Jim Hough <teachlife@outlook.com>
2020-06-04 00:29:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function buildHistogram(array){
+  let hash = {}
+  array.forEach(el => {
+    if (hash[el]){
+      hash[el] += 1
+    }
+    else{
+      hash[el] = 1
+    }
+  })
+  return hash
+}
